0733e8760e56ee43be5a08aa5ea2c98e7eb764ec
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-10-14 19:22:38 +0200

    Add navbar componenet to app and check if humbuger-menu is click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
-import SideBar from './components/SideBar';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './layout/Navbar';
 
 function App() {
+  const [classValue, setClassValue] = useState('show');
+  const [showNav, setShowNav] = useState('block');
+  const toggleMenu = () => {
+    if (classValue === 'show') {
+      setClassValue('hide');
+      setShowNav('none');
+    } else {
+      setClassValue('show');
+      setShowNav('block');
+    }
+  };
   return (
     <div className="App">
-      <SideBar />
+      <Navbar
+          classValue={classValue}
+          toggleMenu={toggleMenu}
+        />
     </div>
   );
 }
