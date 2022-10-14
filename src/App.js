6b1661f65ff874a966eba3302bd68ddd8ee51b4c
bamsi <hibamsi@gmail.com>
2022-10-14 17:27:29 +0300

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Splash from './components/pages/splash';
+import Login from './components/pages/login';
 
 function App() {
   return (
     <div className="container-fluid px-0">
       <Routes>
         <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/components/pages/login.js
@@ -0,0 +1,27 @@
+const Login = () => (
+  <div className="container-img">
+    <div className="d-flex aligns-items-center justify-content-center h-100 d-inline-block">
+      <div className="card align-self-center w-25 p-4">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="password" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+);
+
+export default Login;
