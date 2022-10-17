e6c6af8775b3b623bc9b024b9e8cdf9e9db4f46c
bamsi <hibamsi@gmail.com>
2022-10-17 11:56:57 +0300

    update signup page to send data to API

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -1,42 +1,109 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import HomeButton from '../../layout/homeButton';
 
-const Signup = () => (
-  <div className="container-img">
-    <div className="d-flex aligns-items-center justify-content-center h-100 d-inline-block">
-      <div className="card align-self-center p-4" style={{ width: '28rem' }}>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Username
-            </label>
-            <input type="text" className="form-control" />
+const Signup = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    retypePassword: '',
+  });
+
+  const submitRequest = (event) => {
+    event.preventDefault();
+    const response = {
+      name: values.username,
+      password: values.password,
+    };
+    if (values.password === values.retypePassword) {
+      navigate('/home');
+    }
+  };
+
+  const handleUsernameChange = (e) => {
+    setValues({
+      ...values,
+      username: e.target.value,
+    });
+  };
+
+  const handlePasswordChange = (e) => {
+    setValues({
+      ...values,
+      password: e.target.value,
+    });
+  };
+
+  const handleRetypePasswordChange = (e) => {
+    const retypePassword = e.target.value;
+    if (retypePassword !== values.password) {
+      setError('Password does not match!');
+    }
+    setValues({
+      ...values,
+      retypePassword: e.target.value,
+    });
+  };
+
+  return (
+    <div className="container-img">
+      <div className="d-flex aligns-items-center justify-content-center h-100 d-inline-block">
+        <div className="card align-self-center p-4" style={{ width: '28rem' }}>
+          {error && (
+            <div className="alert alert-danger" role="alert">
+              {error}
+            </div>
+          )}
+          <form onSubmit={submitRequest}>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value={values.username}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={values.password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="retypePassword" className="form-label">
+                Retype Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="retypePassword"
+                value={values.retypePassword}
+                onChange={handleRetypePasswordChange}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Sign up
+            </button>
+          </form>
+          <div className="align-self-end">
+            <HomeButton />
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input type="password" className="form-control" id="password" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="retypePassword" className="form-label">
-              Retype Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="retypePassword"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Sign up
-          </button>
-        </form>
-        <div className="align-self-end">
-          <HomeButton />
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Signup;
