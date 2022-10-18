6c850492525af1237b4034e5267486e2cffa7b28
bamsi <hibamsi@gmail.com>
2022-10-18 12:00:03 +0300

    add genres and categories store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -7,8 +7,15 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import authentication from './authentication/authentication';
 import movies from './movie/movies';
+import categories from './category/categories';
+import genres from './genre/genres';
 
-const rootReducer = combineReducers({ authentication, movies });
+const rootReducer = combineReducers({
+  authentication,
+  movies,
+  categories,
+  genres,
+});
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
