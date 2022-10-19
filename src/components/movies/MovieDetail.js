546d3ea7542aedf9c5c5e1250cf512bfb78a618e
ilham bouaik <ibouaik@gmail.com>
2022-10-19 19:16:38 +0100

    fix response issu

--- src/components/movies/MovieDetail.js
+++ src/components/movies/MovieDetail.js
@@ -6,7 +6,7 @@ import { selectMovie } from '../../redux/movie/movie-detail';
 
 function MovieDetail() {
   const movie = useSelector((state) => state.movie);
-  console.log(movie);
+  // console.log(movie);
   const movieId = useParams();
   const dispatch = useDispatch();
   // console.log(movieId.movieId);
@@ -15,8 +15,8 @@ function MovieDetail() {
     const response = await axios.get(`http://127.0.0.1:3000/api/v1/movies/${movieId.movieId}`).catch((err) => {
       console.log('Error', err);
     });
-    dispatch(selectMovie(response.data));
-    // console.log(response.data);
+    dispatch(selectMovie(...response.data));
+    // console.log(...response.data);
   };
 
   useEffect(() => {
@@ -26,7 +26,11 @@ function MovieDetail() {
   return (
     <div>
       MovieDetail
-      <h1>hello</h1>
+      <h1>
+        {
+          movie.title
+        }
+      </h1>
     </div>
   );
 }
