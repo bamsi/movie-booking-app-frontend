6a046835b5759ae1c74cee7c3ccdead9869e46e8
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-10-20 18:06:12 +0200

    Add test for new booking form

--- src/__test__/MovieBookings.test.js
+++ src/__test__/MovieBookings.test.js
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import MovieBookings from '../components/bookings/MovieBookings';
-import { Route } from 'react-router';
 
 const user ={
     user: {
--- src/components/bookings/NewBooking.js
+++ src/components/bookings/NewBooking.js
@@ -22,7 +22,7 @@ function NewBooking() {
     city: '',
     date: new Date(),
     movie_id: '',
-    user_id: user.user.id,
+    user_id: user?.user?.id,
   });
 
   const handleCityChange = (e) => {
